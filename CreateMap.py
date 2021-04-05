a229e31c299e6c930a4ffa7851a0b456559382e1
wilyub <68028075+wilyub@users.noreply.github.com>
2021-04-05 17:47:34 -0400

    Add Agent 1 method and other minor fixes

--- CreateMap.py
+++ CreateMap.py
@@ -2,14 +2,15 @@ import numpy as np
 import random as ran
 
 class Agent:
-    def __init__(self, start, map):                                                        #Agent receives the map and its starting square as inputs
+    def __init__(self, start, map):                                                     #Agent receives the map and its starting square as inputs
         self.map = map
         self.query = start
         self.x = start[0]
-        self.y = start[1]                          
+        self.y = start[1]     
+        self.score = 0                                                                  #Distance traveled + number of searches                     
 
-class MapSpace:                                                                      #Holds all the info for a map space
-    def __init__(self, dim, query, terrain):                                         #Inputs are the space (query), the current map and dimension
+class MapSpace:                                                                         #Holds all the info for a map space
+    def __init__(self, dim, query, terrain, agent_space):         #Inputs are the space (query), the current map ,dimension, and agent's location
         self.query = query
         self.x = query[0]
         self.y = query[1]
@@ -18,22 +19,30 @@ class MapSpace:                                                                 
         self.misses = 0
         self.init_prob = 1/(dim**2)
         self.prob = 1/(dim**2)
-    def __lt__(self, other):                                                        #Order the fringe based on how close to guarantee
-        return self.prob < other.prob
+        self.manhattan = abs(query[0] - agent_space[0]) + abs(query[1] - agent_space[1])
+    def __lt__(self, other):                                                            #Order the fringe based on how close to guarantee
+        if self.prob == other.prob:
+            return self.manhattan < other.manhattan
+        return self.prob*-1 < other.prob*-1
 
 def create_map(dim):
     map = []
     terrains = [0.1, 0.3, 0.7, 0.9]
+    start_x = ran.randint(0,dim-1)
+    start_y = ran.randint(0,dim-1)
+    start_space = (start_x, start_y)
     for x in range(dim):
         map.append([])
         for y in range(dim):
             t_num = ran.randint(0,3)
-            map[x].append(MapSpace(dim, (x,y), terrains[t_num]))
+            map[x].append(MapSpace(dim, (x,y), terrains[t_num], start_space))
     rand_x = ran.randint(0,dim-1)
     rand_y = ran.randint(0,dim-1)
     map[rand_x][rand_y].target = 1
+    my_agent = Agent(start_space, map)
     print("The ({}, {}) spot holds the target.\n".format(rand_x, rand_y))
-    return map
+    print("The agent starts at ({}, {}).\n".format(start_x, start_y))
+    return [map, my_agent]
 
 def print_map(map):
     dim = len(map)
@@ -52,6 +61,7 @@ def print_terrain(map):
     print(pm)
 
 if __name__ == '__main__':
-    my_map = create_map(5)
+    output = create_map(5)
+    my_map = output[0]
     print_map(my_map)
     print_terrain(my_map)
--- SearchAndDestroy.py
+++ SearchAndDestroy.py
@@ -1,8 +1,10 @@
 import numpy as np
 import random as ran
 import CreateMap
+import heapq
 
-def search_space(space):
+def search_space(space, agent):
+    agent.score += 1                                                                            #Each search increases agent's score
     if not space.target:
         return 0
     if ran.random() > space.terrain:
@@ -10,13 +12,60 @@ def search_space(space):
     else:
         return 0
 
-def prob_formula(prob, dim, miss_rate, n):
-    return (prob)/(1-(1/dim**2)+((1/dim**2)*(miss_rate**n)))
+# def prob_formula(prob, dim, miss_rate, n):
+#     return (prob)/(1-(1/dim**2)+((1/dim**2)*(miss_rate**n)))
 
-def update_map(unsearched_list, map):
-    dim = len(map)
-    for space in unsearched_list:
-        space.prob = prob_formula(space.prob, dim, space.terrain, space.misses)
+# def update_map(unsearched_list, map):
+#     dim = len(map)
+#     for space in unsearched_list:
+#         space.prob = prob_formula(space.prob, dim, space.terrain, space.misses)
+
+def prob_contains(map_space, fail_space, agent):                                                #Calculates P(in cell I | failure cell J)
+    prob_given_fail = map_space.prob / (fail_space.prob*fail_space.terrain+1-fail_space.prob)
+    map_space.prob = prob_given_fail
+    map_space.manhattan = abs(map_space.x - agent.x) + abs(map_space.y - agent.y)
+
+def fail_in_current(fail_space, agent):                                                         #Calculates P(in cell J | failure cell J)
+    new_prob = fail_space.prob * fail_space.terrain / (fail_space.prob * fail_space.terrain + 1 - fail_space.prob)
+    fail_space.prob = new_prob
+    fail_space.manhattan = abs(fail_space.x - agent.x) + abs(fail_space.y - agent.y)
+
+def update_prob_map(fringe, agent, fail_space):
+    for space in fringe:
+        prob_contains(space, fail_space, agent)
+
+def agent_1(dim):
+    creation_output = CreateMap.create_map(dim)
+    environment = creation_output[0]
+    my_agent = creation_output[1]
+    CreateMap.print_terrain(environment)
+    fringe = []                                                                         #Holds all map spaces for probability checking
+    for x in range(dim):
+        for y in range(dim):
+            fringe.append(environment[x][y])
+    while(True):
+        #CreateMap.print_map(environment)
+        heapq.heapify(fringe)
+        goal_search = heapq.heappop(fringe)                                             #Highest probability && Lowest manhattan
+        agent_space = (my_agent.x, my_agent.y)
+        if agent_space == goal_search.query:                                            #Highest prob is current space
+            check = search_space(goal_search, my_agent)                                 #Check if target is found
+            if check:
+                return my_agent.score
+        else:                                                                           #Agent needs to travel
+            my_agent.score += goal_search.manhattan                                     #Distance traveled is manhattan, no need to actually travel for basic agent
+            my_agent.x = goal_search.x                                                  #Update agent's location
+            my_agent.y = goal_search.y
+            check = search_space(goal_search, my_agent)                                 #Check if target is found
+            if check:
+                return my_agent.score
+        fail_in_current(goal_search, my_agent)
+        update_prob_map(fringe, my_agent, goal_search)
+        fringe.append(goal_search)
+            
+if __name__ == '__main__':
+    score = agent_1(50)
+    print("Your score is {}.\n".format(score))
 
 
 
