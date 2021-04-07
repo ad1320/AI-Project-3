61c1cb16d2f43b30eb90b03f2da9bdf92dec4dbe
ad1320 <ad1320@rutgers.edu>
2021-04-07 17:13:24 -0400

    stats stuff and very basic agent 3, rn its worse than nothing but probably improves with small tweak

--- CreateMap.py
+++ CreateMap.py
@@ -9,7 +9,7 @@ class Agent:
         self.y = start[1]     
         self.score = 0                                                                  #Distance traveled + number of searches                     
 
-class MapSpace:                                                                         #Holds all the info for a map space
+class MapSpace_1:                                                                         #Holds all the info for a map space
     def __init__(self, dim, query, terrain, agent_space):         #Inputs are the space (query), the current map ,dimension, and agent's location
         self.query = query
         self.x = query[0]
@@ -25,7 +25,37 @@ class MapSpace:                                                                 
             return self.manhattan < other.manhattan
         return self.prob*-1 < other.prob*-1
 
-def create_map(dim):
+class MapSpace_2:                                                                         #Holds all the info for a map space
+    def __init__(self, dim, query, terrain, agent_space):         #Inputs are the space (query), the current map ,dimension, and agent's location
+        self.query = query
+        self.x = query[0]
+        self.y = query[1]
+        self.terrain = terrain
+        self.target = 0
+        self.misses = 0
+        self.init_prob = 1/(dim**2)
+        self.prob = 1/(dim**2)
+        self.manhattan = abs(query[0] - agent_space[0]) + abs(query[1] - agent_space[1])
+    def __lt__(self, other):                                                            #Order the fringe based on how close to guarantee
+        if self.prob*(1-self.terrain) == other.prob*(1-other.terrain):
+            return self.manhattan < other.manhattan
+        return self.prob*(1-self.terrain)*-1 <other.prob*(1-other.terrain)*-1
+
+class MapSpace_3:                                                                         #Holds all the info for a map space
+    def __init__(self, dim, query, terrain, agent_space):         #Inputs are the space (query), the current map ,dimension, and agent's location
+        self.query = query
+        self.x = query[0]
+        self.y = query[1]
+        self.terrain = terrain
+        self.target = 0
+        self.misses = 0
+        self.init_prob = 1/(dim**2)
+        self.prob = 1/(dim**2)
+        self.manhattan = abs(query[0] - agent_space[0]) + abs(query[1] - agent_space[1])
+    def __lt__(self, other):                                                            #Order the fringe based on how close to guarantee
+        return self.prob*self.manhattan*-1 < other.prob*other.manhattan*-1
+
+def create_map_1(dim):
     map = []
     terrains = [0.1, 0.3, 0.7, 0.9]
     start_x = ran.randint(0,dim-1)
@@ -35,7 +65,7 @@ def create_map(dim):
         map.append([])
         for y in range(dim):
             t_num = ran.randint(0,3)
-            map[x].append(MapSpace(dim, (x,y), terrains[t_num], start_space))
+            map[x].append(MapSpace_1(dim, (x,y), terrains[t_num], start_space))
     rand_x = ran.randint(0,dim-1)
     rand_y = ran.randint(0,dim-1)
     map[rand_x][rand_y].target = 1
@@ -43,6 +73,84 @@ def create_map(dim):
     print("The ({}, {}) spot holds the target.\n".format(rand_x, rand_y))
     print("The agent starts at ({}, {}).\n".format(start_x, start_y))
     return [map, my_agent]
+
+def create_map_2(dim):
+    map = []
+    terrains = [0.1, 0.3, 0.7, 0.9]
+    start_x = ran.randint(0,dim-1)
+    start_y = ran.randint(0,dim-1)
+    start_space = (start_x, start_y)
+    for x in range(dim):
+        map.append([])
+        for y in range(dim):
+            t_num = ran.randint(0,3)
+            map[x].append(MapSpace_2(dim, (x,y), terrains[t_num], start_space))
+    rand_x = ran.randint(0,dim-1)
+    rand_y = ran.randint(0,dim-1)
+    map[rand_x][rand_y].target = 1
+    my_agent = Agent(start_space, map)
+    print("The ({}, {}) spot holds the target.\n".format(rand_x, rand_y))
+    print("The agent starts at ({}, {}).\n".format(start_x, start_y))
+    return [map, my_agent]
+
+def create_map_3(dim):
+    map = []
+    terrains = [0.1, 0.3, 0.7, 0.9]
+    start_x = ran.randint(0,dim-1)
+    start_y = ran.randint(0,dim-1)
+    start_space = (start_x, start_y)
+    for x in range(dim):
+        map.append([])
+        for y in range(dim):
+            t_num = ran.randint(0,3)
+            map[x].append(MapSpace_3(dim, (x,y), terrains[t_num], start_space))
+    rand_x = ran.randint(0,dim-1)
+    rand_y = ran.randint(0,dim-1)
+    map[rand_x][rand_y].target = 1
+    my_agent = Agent(start_space, map)
+    print("The ({}, {}) spot holds the target.\n".format(rand_x, rand_y))
+    print("The agent starts at ({}, {}).\n".format(start_x, start_y))
+    return [map, my_agent]
+
+def create_all_maps(dim):
+    maps = [[[],[]],[[],[]],[[],[]]]
+    terrains = [0.1, 0.3, 0.7, 0.9]
+    start_x = ran.randint(0,dim-1)
+    start_y = ran.randint(0,dim-1)
+    start_space = (start_x, start_y)
+    rand_x = ran.randint(0,dim-1)
+    rand_y = ran.randint(0,dim-1)
+
+    for x in range(dim):
+        maps[0][0].append([])
+        for y in range(dim):
+            t_num = ran.randint(0,3)
+            maps[0][0][x].append(MapSpace_1(dim, (x,y), terrains[t_num], start_space))
+    maps[0][0][rand_x][rand_y].target = 1
+    my_agent_1 = Agent(start_space, maps[0][0])
+    maps[0][1] = my_agent_1
+
+    for x in range(dim):
+        maps[1][0].append([])
+        for y in range(dim):
+            maps[1][0][x].append(MapSpace_2(dim, (x,y), maps[0][0][x][y].terrain, start_space))
+    maps[1][0][rand_x][rand_y].target = 1
+    my_agent_2 = Agent(start_space, maps[1][0])
+    maps[1][1] = my_agent_2
+
+    for x in range(dim):
+        maps[2][0].append([])
+        for y in range(dim):
+            t_num = ran.randint(0,3)
+            maps[2][0][x].append(MapSpace_3(dim, (x,y), maps[0][0][x][y].terrain, start_space))
+    maps[2][0][rand_x][rand_y].target = 1
+    my_agent_3 = Agent(start_space, maps[2][0])
+    maps[2][1] = my_agent_3
+    
+    print("The ({}, {}) spot holds the target.\n".format(rand_x, rand_y))
+    print("The agent starts at ({}, {}).\n".format(start_x, start_y))
+    return maps
+
 
 def print_map(map):
     dim = len(map)
@@ -61,7 +169,7 @@ def print_terrain(map):
     print(pm)
 
 if __name__ == '__main__':
-    output = create_map(5)
+    output = create_map_3(5)
     my_map = output[0]
     print_map(my_map)
     print_terrain(my_map)
--- SearchAndDestroy.py
+++ SearchAndDestroy.py
@@ -2,14 +2,18 @@ import numpy as np
 import random as ran
 import CreateMap
 import heapq
+import math
+import matplotlib.pyplot as plt
 
 def search_space(space, agent):
     agent.score += 1                                                                            #Each search increases agent's score
     if not space.target:
+        space.misses+=1
         return 0
     if ran.random() > space.terrain:
         return 1
     else:
+        space.misses+=1
         return 0
 
 # def prob_formula(prob, dim, miss_rate, n):
@@ -35,10 +39,10 @@ def update_prob_map(fringe, agent, fail_space):
         prob_contains(space, fail_space, agent)
 
 def agent_1(dim):
-    creation_output = CreateMap.create_map(dim)
+    creation_output = CreateMap.create_map_1(dim)
     environment = creation_output[0]
     my_agent = creation_output[1]
-    CreateMap.print_terrain(environment)
+    #CreateMap.print_terrain(environment)
     fringe = []                                                                         #Holds all map spaces for probability checking
     for x in range(dim):
         for y in range(dim):
@@ -60,14 +64,179 @@ def agent_1(dim):
             check = search_space(goal_search, my_agent)                                 #Check if target is found
             if check:
                 CreateMap.print_map(environment)
+                print("It took {} searches in {} terrain {} to hit the target.\n".format(goal_search.misses+1, goal_search.terrain, goal_search.query))
+                return my_agent.score
+        fail_in_current(goal_search, my_agent)
+        update_prob_map(fringe, my_agent, goal_search)
+        fringe.append(goal_search)
+
+def agent_1_dep(dim, environment, my_agent):
+    #CreateMap.print_terrain(environment)
+    fringe = []                                                                         #Holds all map spaces for probability checking
+    for x in range(dim):
+        for y in range(dim):
+            fringe.append(environment[x][y])
+    while(True):
+        #CreateMap.print_map(environment)
+        heapq.heapify(fringe)
+        goal_search = heapq.heappop(fringe)                                             #Highest probability && Lowest manhattan
+        agent_space = (my_agent.x, my_agent.y)
+        if agent_space == goal_search.query:                                            #Highest prob is current space
+            check = search_space(goal_search, my_agent)                                 #Check if target is found
+            if check:
+                CreateMap.print_map(environment)
+                return my_agent.score
+        else:                                                                           #Agent needs to travel
+            my_agent.score += goal_search.manhattan                                     #Distance traveled is manhattan, no need to actually travel for basic agent
+            my_agent.x = goal_search.x                                                  #Update agent's location
+            my_agent.y = goal_search.y
+            check = search_space(goal_search, my_agent)                                 #Check if target is found
+            if check:
+                CreateMap.print_map(environment)
+                print("It took {} searches in {} terrain {} to hit the target.\n".format(goal_search.misses+1, goal_search.terrain, goal_search.query))
+                return my_agent.score
+        fail_in_current(goal_search, my_agent)
+        update_prob_map(fringe, my_agent, goal_search)
+        fringe.append(goal_search)
+
+def agent_2(dim):
+    creation_output = CreateMap.create_map_2(dim)
+    environment = creation_output[0]
+    my_agent = creation_output[1]
+    #CreateMap.print_terrain(environment)
+    fringe = []                                                                         #Holds all map spaces for probability checking
+    for x in range(dim):
+        for y in range(dim):
+            fringe.append(environment[x][y])
+    while(True):
+        #CreateMap.print_map(environment)
+        heapq.heapify(fringe)
+        goal_search = heapq.heappop(fringe)                                             #Highest probability && Lowest manhattan
+        agent_space = (my_agent.x, my_agent.y)
+        if agent_space == goal_search.query:                                            #Highest prob is current space
+            check = search_space(goal_search, my_agent)                                 #Check if target is found
+            if check:
+                CreateMap.print_map(environment)
+                return my_agent.score
+        else:                                                                           #Agent needs to travel
+            my_agent.score += goal_search.manhattan                                     #Distance traveled is manhattan, no need to actually travel for basic agent
+            my_agent.x = goal_search.x                                                  #Update agent's location
+            my_agent.y = goal_search.y
+            check = search_space(goal_search, my_agent)                                 #Check if target is found
+            if check:
+                CreateMap.print_map(environment)
+                print("It took {} searches in {} terrain {} to hit the target.\n".format(goal_search.misses+1, goal_search.terrain, goal_search.query))
+                return my_agent.score
+        fail_in_current(goal_search, my_agent)
+        update_prob_map(fringe, my_agent, goal_search)
+        fringe.append(goal_search)
+
+def agent_2_dep(dim, environment, my_agent):
+    #CreateMap.print_terrain(environment)
+    fringe = []                                                                         #Holds all map spaces for probability checking
+    for x in range(dim):
+        for y in range(dim):
+            fringe.append(environment[x][y])
+    while(True):
+        #CreateMap.print_map(environment)
+        heapq.heapify(fringe)
+        goal_search = heapq.heappop(fringe)                                             #Highest probability && Lowest manhattan
+        agent_space = (my_agent.x, my_agent.y)
+        if agent_space == goal_search.query:                                            #Highest prob is current space
+            check = search_space(goal_search, my_agent)                                 #Check if target is found
+            if check:
+                CreateMap.print_map(environment)
+                return my_agent.score
+        else:                                                                           #Agent needs to travel
+            my_agent.score += goal_search.manhattan                                     #Distance traveled is manhattan, no need to actually travel for basic agent
+            my_agent.x = goal_search.x                                                  #Update agent's location
+            my_agent.y = goal_search.y
+            check = search_space(goal_search, my_agent)                                 #Check if target is found
+            if check:
+                CreateMap.print_map(environment)
+                print("It took {} searches in {} terrain {} to hit the target.\n".format(goal_search.misses+1, goal_search.terrain, goal_search.query))
+                return my_agent.score
+        fail_in_current(goal_search, my_agent)
+        update_prob_map(fringe, my_agent, goal_search)
+        fringe.append(goal_search)
+
+def agent_3(dim):
+    creation_output = CreateMap.create_map_3(dim)
+    environment = creation_output[0]
+    my_agent = creation_output[1]
+    #CreateMap.print_terrain(environment)
+    fringe = []                                                                         #Holds all map spaces for probability checking
+    for x in range(dim):
+        for y in range(dim):
+            fringe.append(environment[x][y])
+    while(True):
+        #CreateMap.print_map(environment)
+        heapq.heapify(fringe)
+        goal_search = heapq.heappop(fringe)                                             #Highest probability && Lowest manhattan
+        agent_space = (my_agent.x, my_agent.y)
+        if agent_space == goal_search.query:                                            #Highest prob is current space
+            check = search_space(goal_search, my_agent)                                 #Check if target is found
+            if check:
+                CreateMap.print_map(environment)
+                return my_agent.score
+        else:                                                                           #Agent needs to travel
+            my_agent.score += goal_search.manhattan                                     #Distance traveled is manhattan, no need to actually travel for basic agent
+            my_agent.x = goal_search.x                                                  #Update agent's location
+            my_agent.y = goal_search.y
+            check = search_space(goal_search, my_agent)                                 #Check if target is found
+            if check:
+                CreateMap.print_map(environment)
+                print("It took {} searches in {} terrain {} to hit the target.\n".format(goal_search.misses+1, goal_search.terrain, goal_search.query))
+                return my_agent.score
+        fail_in_current(goal_search, my_agent)
+        update_prob_map(fringe, my_agent, goal_search)
+        fringe.append(goal_search)
+
+def agent_3_dep(dim, environment, my_agent):
+    creation_output = CreateMap.create_map_3(dim)
+    #CreateMap.print_terrain(environment)
+    fringe = []                                                                         #Holds all map spaces for probability checking
+    for x in range(dim):
+        for y in range(dim):
+            fringe.append(environment[x][y])
+    while(True):
+        #CreateMap.print_map(environment)
+        heapq.heapify(fringe)
+        goal_search = heapq.heappop(fringe)                                             #Highest probability && Lowest manhattan
+        agent_space = (my_agent.x, my_agent.y)
+        if agent_space == goal_search.query:                                            #Highest prob is current space
+            check = search_space(goal_search, my_agent)                                 #Check if target is found
+            if check:
+                CreateMap.print_map(environment)
+                return my_agent.score
+        else:                                                                           #Agent needs to travel
+            my_agent.score += goal_search.manhattan                                     #Distance traveled is manhattan, no need to actually travel for basic agent
+            my_agent.x = goal_search.x                                                  #Update agent's location
+            my_agent.y = goal_search.y
+            check = search_space(goal_search, my_agent)                                 #Check if target is found
+            if check:
+                CreateMap.print_map(environment)
+                print("It took {} searches in {} terrain {} to hit the target.\n".format(goal_search.misses+1, goal_search.terrain, goal_search.query))
                 return my_agent.score
         fail_in_current(goal_search, my_agent)
         update_prob_map(fringe, my_agent, goal_search)
         fringe.append(goal_search)
             
+def compare_agents(agent_1, agent_2, agent_3, dim, trials):
+    count_1 = 0
+    count_2 = 0
+    count_3 = 0
+    for y in range(trials):                                            #Perform 100 tests per density
+        maps = CreateMap.create_all_maps(dim)
+        count_1 += agent_1_dep(dim, maps[0][0], maps[0][1])
+        count_2 += agent_2_dep(dim, maps[1][0], maps[1][1])                                      
+        count_3 += agent_3_dep(dim, maps[2][0], maps[2][1])
+    print(count_1, count_2, count_3)
+
 if __name__ == '__main__':
-    score = agent_1(5)
-    print("Your score is {}.\n".format(score))
+    #score = agent_2(10)
+    #print("Your score is {}.\n".format(score))
+    compare_agents(agent_1, agent_2, agent_3, 10, 100)
 
 
 
